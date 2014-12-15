df0405d7ac6bd2e2777bccb003d207f95ed0ee23
Andrew Reading-Legh <arpreading@live.co.uk>
2014-12-15 20:47:26 +0000

    CW4 Attempt 1 additional comments

--- ComplexCalculator.py
+++ ComplexCalculator.py
@@ -1,11 +1,15 @@
 ## areadi01 - ISD2014 CW4
 #
+# My PsuedoCode interpretation of the problem:
+#
+# Take a string from the user,
+# Split it into parts such that 'int1,int2 j' is returned as complex(int1,int2)
+# Separate out any other whole numbers calculate as complex(wholeNumber) so that it is returned in the form (n+0j)
+# Perform calculation according to the operator(s) included in the string.  Return the value. Repeat if prompted or exit
+#
 # Difficulties with the problem:
 #
 # 1) splitting the string in the correct format so that it can be worked with properly.
-# Take a string, split it into parts such that 'int1,int2 j' is returned as complex(int1,int2)
-# separate out any other whole numbers complex(wholeNumber) so that it is returned in the form (n+0j)
-# Perform calculation according to the operator(s) included in the string.  Return the value.
 #
 # 2) using try/except to raise the exceptions to print a message
 #
@@ -15,7 +19,7 @@
 #   but way beyond my understanding at present.
 
 
-#functions for performing calculations
+#functions for performing calculations (maybe would be placed in another function at a later date)
 def add(firstNumber,secondNumber):
     return firstNumber+secondNumber
 
@@ -28,7 +32,7 @@ def multiply(firstNumber,secondNumber):
 def divide(firstNumber,secondNumber):
     return float(firstNumber)/float(secondNumber)
 
-#function for splitting the string
+#function for splitting the string (would be better to use split or re.split, as numbers and operators doesn't really help)
 def calcInput(s):
     numbers = "0123456789."
     operators = []
@@ -47,27 +51,28 @@ def calcInput(s):
         integers.append(last_number)
     return operators + integers
 
-# function for evaluating user input
+# function for evaluating user input as prompted by the problem set
 def evaluate(complexnumber, inputString):
     calc_val = 0
     stop = {'q','Q','quit'}
     clear = {'c','C','clear'}
-    for char in inputString:
+    for char in inputString: #needs to be try/except (print message), rather than 'hard' exceptions
             if char in stop:
                 raise EOFError ("Goodbye!")
             elif char in clear:
                 calc_val = 0
+            #elif:     this should either be a call to the calculator function or process the input here
             else:
                 raise ValueError ("Error!")
                 
 
 # main function of program
 def main():
-    complexnumber = 3,4j
+    complexnumber = 3,4j # would be good for this to get its value from the first complex number found in the user input
     print("Welcome to the complex calculator")
     while True:
         user1 = input("Please enter your input: ")
-        evaluate (complexnumber, user1)
+        evaluate (complexnumber, user1)# This is where the magic should happen.
         new_calc = input("Would you like to know more? 'yes/no' ")
         if new_calc != "yes":
             print("Goodbye")
@@ -75,4 +80,6 @@ def main():
         else:
             continue
 
-main()        
+if __name__ == '__main__':
+    main()
+    
